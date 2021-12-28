9251da2f12daef0c536a1d07d9cc6053122ac6ab
[Digvikadam] <[digvikadam168@gmail.com]>
2021-12-28 16:27:01 +0530

    Added: Dequeue From the Beginning

--- src/com/bridgelabz/stackandqueue/StackAndQueue.java
+++ src/com/bridgelabz/stackandqueue/StackAndQueue.java
@@ -4,14 +4,15 @@ public class StackAndQueue {
 	Node head;
 	Node tail;
 	Node top;
-	
+
 	public Node push(int data) {
 		Node newNode = new Node(data);
-		
+
 		if (head == null) {
 			head = newNode;
 			tail = newNode;
-		} else {
+		} 
+		else {
 			Node tempNode = head;
 			this.head = newNode;
 			newNode.next = tempNode;
@@ -19,7 +20,7 @@ public class StackAndQueue {
 		top=head;
 		return newNode;
 	}
-	
+
 	public int peek(){
 		System.out.println("Top element is "+this.top.data);
 		return top.data;
@@ -32,7 +33,6 @@ public class StackAndQueue {
 			this.head = tempNode.next;
 			temp=temp.next;
 		}
-
 	}
 
 	public void queue(int data) {
@@ -41,18 +41,20 @@ public class StackAndQueue {
 			head = newNode;
 			tail = newNode;
 			top=head;
-		} else {
+		} 
+		else {
 			Node tempNode=tail;
 			this.tail=newNode;
 			tempNode.next=tail;
 		}
 	}
-	
+
 	public void display(){
 		if (head == null) {
 			System.out.println("Linked list is Empty");
 			return;
-		} else {
+		} 
+		else {
 			System.out.print("\n The stack elements are ");
 			Node tempNode = head;
 			while (tempNode != null) {
--- src/com/bridgelabz/stackandqueue/StackAndQueueMain.java
+++ src/com/bridgelabz/stackandqueue/StackAndQueueMain.java
@@ -4,25 +4,28 @@ import java.util.Scanner;
 
 public class StackAndQueueMain {
 	public static void main(String[] args) {
-        Operation operation = new Operation();
-        
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter 1. To add element at the start \n" + "Enter 2. To show the peek element\n" +
-        		"Enter 3. To show the pop \n " + "Enter 4. To queue the element");
-        switch (scanner.nextInt()) {
-        case 1:
-        	operation.addElement();
-        	break;
-        case 2:
-        	operation.peek();
-        	break;
-        case 3:
-        	operation.pop();
-        	break;
-        case 4:
-        	operation.queueElement();
-        	break;
-        }
-        scanner.close();
+		Operation operation = new Operation();
+			
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter 1. To add element at the start \n" + "Enter 2. To show the peek element\n" +
+				"Enter 3. To show the pop \n " + "Enter 4. To queue the element\n" + "Enter 5. To dequeue the element");
+		switch (scanner.nextInt()) {
+		case 1:
+			operation.addElement();
+			break;
+		case 2:
+			operation.peek();
+			break;
+		case 3:
+			operation.pop();
+			break;
+		case 4:
+			operation.queueElement();
+			break;
+		case 5:
+			operation.dequeueElement();
+			break;
+		}
+		scanner.close();
 	}
 }
